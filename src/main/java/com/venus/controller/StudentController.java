72433ca1206a7838c4b331abd638375b7a177a5b
杨恺 <njustyangkai@126.com>
2016-10-12 00:01:19 +0800

    ss

--- src/main/java/com/venus/controller/StudentController.java
+++ src/main/java/com/venus/controller/StudentController.java
@@ -6,13 +6,11 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
 import com.venus.frame.utils.ResultBean;
 import com.venus.pojo.StudentInfo;
 import com.venus.service.StudentService;
@@ -28,7 +26,7 @@ public class StudentController {
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
-	public Object getStudents(@RequestBody JSONObject params) {
+	public Object getStudents() {
 		try {
 			List<StudentInfo> students = new ArrayList<StudentInfo>();
 			students = studentService.getStudents();
